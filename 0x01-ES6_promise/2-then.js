1170e12b0f7180415ebb60d7e992b314641a10c3
Haile-08 <haile4cmd@gmail.com>
2023-01-30 12:59:27 +0000

    For every resolution, log Got a response from the API to the console

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+	const res = "Got a response from the API";
+	return promise.then(()=> ({
+		status: 200,
+		body: 'success',
+	})).catch(() => (
+		new Error()
+	)).finally(() => console.log(res));
+}
